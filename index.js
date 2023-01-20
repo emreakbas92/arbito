4628a87045be5a086149e137e5aa72bd92d1ea4e
emreakbas92 <99279212+emreakbas92@users.noreply.github.com>
2023-01-20 09:27:31 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -130,7 +130,3 @@ app.listen(3000, () => {
   res.send('AL: ${al} - SAT: ${sat}');
   });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
