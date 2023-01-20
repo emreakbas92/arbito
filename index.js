6cb2f66bb2b7b5837b3008aedbc5151a36036065
emreakbas92 <99279212+emreakbas92@users.noreply.github.com>
2023-01-20 15:07:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -137,4 +137,11 @@ app.get("/", (req, res) => {
   `);
 });
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+  }); 
