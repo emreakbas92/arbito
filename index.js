799c1a792a823c6bd0fbd62df5d2e051c60634c3
emreakbas92 <99279212+emreakbas92@users.noreply.github.com>
2023-01-25 10:39:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -99,48 +99,34 @@ setInterval(() => {
           if(!json.tick || !json.tick.ask[0] || !json.tick.bid[0]) return;
           const ask = json.tick.ask[0];
           const bid = json.tick.bid[0];
-
-          // Get the price of the token on the BSC network from Dex.guru
-          https.get(`https://api.dex.guru/v1/tokens/${token.contract}`, (res) => {
+          https.get(`https://api.bybit.com/spot/quote/v1/ticker/24hr?symbol=${token.symbol}`, (res) => {
             let data = "";
             res.on("data", (chunk) => {
               data += chunk;
             });
             res.on("end", () => {
               const json = JSON.parse(data);
-              let price = json.priceUSD;
-              // Get the price of the token on the BSC network from Jup.ag
-              https.get(`https://price.jup.ag/v1/price?id=${token.contract}`, (res) => {
+              if(!json.result || !json.result.bestAskprice || !json.result.bestBidprice) return;
+              let bybit_ask = parseFloat(json.result.bestAskprice);
+              let bybit_bid = parseFloat(json.result.bestBidprice);
+              // Get the price of the token on the BSC network from Dex.guru
+              https.get(`https://api.dex.guru/v1/tokens/${token.contract}`, (res) => {
                 let data = "";
                 res.on("data", (chunk) => {
                   data += chunk;
                 });
                 res.on("end", () => {
                   const json = JSON.parse(data);
-                  let jupPrice = json.data.price;
-                  // Calculate the ratio of the Huobi ask price to the BSC price
-                  token.al_dex = price / bid;
-                  token.al_jup = jupPrice / bid;
-                  token.sat_dex = price / ask;
-                  token.sat_jup = jupPrice / ask;
-                  console.log(token);
-                  // Get the ask and bid prices for the token from Bybit
-                  https.get(`https://api.bybit.com/spot/quote/v1/ticker/24hr?symbol=${token.symbol}`, (res) => {
+                  let price = json.priceUSD;
+                  // Get the price of the token on the BSC network from Jup.ag
+                  https.get(`https://price.jup.ag/v1/price?id=${token.contract}`, (res) => {
                     let data = "";
                     res.on("data", (chunk) => {
                       data += chunk;
                     });
                     res.on("end", () => {
                       const json = JSON.parse(data);
-                      if(!json.result || !json.result.bestAskprice || !json.result.bestBidprice) return;
-                      let bybit_ask = parseFloat(json.result.bestAskprice);
-                      let bybit_bid = parseFloat(json.result.bestBidprice);
-                      // Calculate the ratio of the Bybit ask price to the BSC price
-                      token.al_bybit = price / bybit_bid;
-                      token.sat_bybit = price / bybit_ask;
-                      token.al_jupbybit = jupPrice / bybit_bid;
-                      token.sat_jupbybit = jupPrice / bybit_ask;
-                      console.log(token);
+                      let jupPrice = json.data.price;
                     });
                   });
                 });
@@ -149,11 +135,11 @@ setInterval(() => {
           });
         });
       });
-     } catch (err) {
-     console.log("Error: " + err.message);
+    } catch (err) {
+      console.log(err);
     }
- });
- }, 30000);
+  });
+}, 1000);
 
 
 app.get("/", (req, res) => {
